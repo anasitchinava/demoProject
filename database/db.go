609d900c0608c4c1800a26b094ece3bab610936e
Ana Sitchinava <80534875+anasitchinava@users.noreply.github.com>
2024-10-10 14:11:48 +0400

    Update db.go

--- database/db.go
+++ database/db.go
@@ -7,7 +7,7 @@ import (
 )
 
 func InitDB() *pgxpool.Pool {
-	dbUrl := "postgres://testpsql:root@localhost:5432/test"
+	dbUrl := "postgres://sa:root@localhost:5432/test"
     dbPool, err := pgxpool.Connect(context.Background(), dbUrl)
     if err != nil {
         log.Fatalf("Unable to connect to database: %v\n", err)
